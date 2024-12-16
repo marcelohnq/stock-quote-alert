6eeaa40a1a940889ea54edcc0d467877fe4d7565
Marcelo Henrique <henrick939@gmail.com>
2024-12-16 19:33:36 -0300

    feature: add project infrastructure and ef configurations

---
+++ src/StockQuote.Infrastructure/Data/Config/QuoteConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StockQuote.Core.QuoteAggregate;
+
+namespace StockQuote.Infrastructure.Data.Config;
+
+public class QuoteConfiguration : IEntityTypeConfiguration<Quote>
+{
+    public void Configure(EntityTypeBuilder<Quote> builder)
+    {
+        var navigation = builder.Metadata.FindNavigation(nameof(Quote.Prices));
+        navigation?.SetPropertyAccessMode(PropertyAccessMode.Field);
+
+        builder.OwnsOne(q => q.Asset);
+        builder.OwnsOne(q => q.LimitAlert);
+        builder.OwnsOne(q => q.Audit);
+    }
+}
+
---
+++ src/StockQuote.Infrastructure/Data/Config/QuotePriceConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StockQuote.Core.QuoteAggregate;
+
+namespace StockQuote.Infrastructure.Data.Config;
+
+public class QuotePriceConfiguration : IEntityTypeConfiguration<QuotePrice>
+{
+    public void Configure(EntityTypeBuilder<QuotePrice> builder)
+    {
+        builder.OwnsOne(qp => qp.Price);
+    }
+}
