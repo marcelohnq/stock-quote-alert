83efa7bf1094217b7888b29662e93b7562da43cb
Marcelo Henrique <henrick939@gmail.com>
2024-12-16 19:40:52 -0300

    feature: add appsettings and integrates the console with the other projects

---
+++ src/StockQuote.Console/Configuration/MediatrConfigs.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StockQuote.Core.QuoteAggregate;
+using System.Reflection;
+
+namespace StockQuote.Console.Configuration;
+
+public static class MediatrConfigs
+{
+    public static IServiceCollection AddMediatrConfigs(this IServiceCollection services)
+    {
+        var mediatRAssemblies = new[]
+        {
+            Assembly.GetAssembly(typeof(Quote)), // Core
+        };
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(mediatRAssemblies!));
+
+        return services;
+    }
+}
--- src/StockQuote.Console/Program.cs
+++ src/StockQuote.Console/Program.cs
@@ -1,7 +1,26 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using StockQuote.Console.Configuration;
+using StockQuote.Infrastructure;
 
-HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true);
+
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+
+var logger = builder.Logging.Services.BuildServiceProvider()
+    .GetRequiredService<ILoggerFactory>()
+    .CreateLogger<Program>();
+
+logger.LogInformation("Start - StockQuote sendo iniciado!");
+
+builder.Services.AddMediatrConfigs();
+builder.Services.AddInfrastructureServices(builder.Configuration, logger);
 
 using IHost host = builder.Build();
 
